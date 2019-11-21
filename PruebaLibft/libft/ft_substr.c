653ea4485556832da17807ebd01a7f951e8ef06d
Daniel Tellez llinas <dtellez-@c2r12s4.42madrid.com>
2019-11-21 02:58:32 +0100

    asd

--- PruebaLibft/libft/ft_substr.c
+++ PruebaLibft/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dtellez- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 09:25:57 by dtellez-          #+#    #+#             */
-/*   Updated: 2019/11/21 02:32:25 by dtellez-         ###   ########.fr       */
+/*   Updated: 2019/11/21 02:57:58 by dtellez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char *string;
-	unsigned int i;
+	char			*string;
+	unsigned int	i;
 
 	if (s == NULL)
 		return (NULL);
-	if (start > ft_strlen(s))
-	{
-		return (NULL);
-	}
 	i = 0;
 	if (!(string = (char *)malloc((sizeof(*s) * len) + 1)))
 		return (NULL);
+	ft_bzero(string, len);
+	if (start > ft_strlen(s))
+	{
+		return (string);
+	}
 	while (len--)
 	{
 		string[i] = s[start];
