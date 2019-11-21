b5c46b2a4212be95d6bfe9dec205151820614f90
Daniel Tellez llinas <dtellez-@c2r10s1.42madrid.com>
2019-11-21 19:49:12 +0100

    asd

--- PruebaLibft/libft/libft.h
+++ PruebaLibft/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dtellez- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 13:24:44 by dtellez-          #+#    #+#             */
-/*   Updated: 2019/11/21 18:35:35 by dtellez-         ###   ########.fr       */
+/*   Updated: 2019/11/21 19:48:42 by dtellez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,8 @@ char			*ft_strdup(const char *s1);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
 #endif
