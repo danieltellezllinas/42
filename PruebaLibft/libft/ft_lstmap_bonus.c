8319ce208df0df7061dbf014a908a5bbc51130f5
Daniel Tellez llinas <dtellez-@c2r10s2.42madrid.com>
2019-11-25 21:42:17 +0100

    asd

--- PruebaLibft/libft/ft_lstmap_bonus.c
+++ PruebaLibft/libft/ft_lstmap_bonus.c
@@ -1,26 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dtellez- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/24 20:33:46 by dtellez-          #+#    #+#             */
-/*   Updated: 2019/11/25 21:09:30 by dtellez-         ###   ########.fr       */
+/*   Created: 2019/11/25 21:09:40 by dtellez-          #+#    #+#             */
+/*   Updated: 2019/11/25 21:28:55 by dtellez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_lstsize(t_list *lst)
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	int i;
+	t_list	*new;
+	t_list *tmp;
+	t_list *begin;
 
-	i = 0;
-	while (lst != NULL)
+	if (lst)
 	{
-		lst = lst->next;
-		i++;
+		tmp = lst;
+		if (!(begin = ft_lstnew(f(tmp->content))))
+			return (NULL);
+		tmp = tmp->next;
+		while (tmp)
+		{
+			if (!(new = ft_lstnew(f(tmp->content))))
+			{
+				ft_lstclear(&begin, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&begin, new);
+			tmp = tmp->next;
+		}
+		return (begin);
 	}
-	return (i);
+	return (NULL);
 }
--- PruebaLibft/libft/ft_strtrim.c
+++ PruebaLibft/libft/ft_strtrim.c
@@ -5,92 +5,53 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dtellez- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/21 12:14:10 by dtellez-          #+#    #+#             */
-/*   Updated: 2019/11/24 14:27:08 by dtellez-         ###   ########.fr       */
+/*   Created: 2019/11/25 21:41:15 by dtellez-          #+#    #+#             */
+/*   Updated: 2019/11/25 21:41:19 by dtellez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-static int		ft_istrlen(const char *s)
+static size_t	ft_clean(char c, char *set)
 {
-	int	i;
+	size_t		i;
+	size_t		lenset;
 
 	i = 0;
-	while (s[i])
+	lenset = ft_strlen(set);
+	while (i < lenset)
 	{
+		if (c == set[i])
+			return (1);
 		i++;
 	}
-	return (i);
+	return (0);
 }
 
-static int		start(char const *s1, char const *set)
+char			*ft_strtrim(char const *s1, char const *s2)
 {
-	int i;
-	int a;
+	size_t	i;
+	size_t	j;
+	size_t	len;
+	char	*strtrim;
+	char	*set;
 
+	set = (char*)s2;
 	i = 0;
-	while (s1[i])
-	{
-		a = 0;
-		while (set[a])
-		{
-			if (s1[i] == set[a])
-				break ;
-			a++;
-		}
-		if (set[a] == '\0')
-			break ;
+	j = 0;
+	if (!s1 || !s2)
+		return (0);
+	len = ft_strlen(s1);
+	while (ft_clean(s1[i], set))
 		i++;
-	}
-	return (i);
-}
-
-static int		end(char const *s1, char const *set)
-{
-	int i;
-	int a;
-
-	i = ft_istrlen(s1) - 1;
-	while (i >= 0)
-	{
-		a = 0;
-		while (set[a])
-		{
-			if (s1[i] == set[a])
-				break ;
-			a++;
-		}
-		if (set[a] == '\0')
-			break ;
-		i--;
-	}
-	return (i);
-}
-
-char			*ft_strtrim(char const *s1, char const *set)
-{
-	char	*string;
-	int		fstart;
-	int		fend;
-	int		i;
-
-	if (s1 == 0 || set == 0)
-		return (NULL);
-	fstart = start(s1, set);
-	fend = end(s1, set);
-	if (fstart > fend)
-		string = (char *)malloc(1 * sizeof(char));
-	else if (!(string = (char *)malloc((fend - fstart + 2) * sizeof(char))))
-		return (NULL);
-	i = 0;
-	while (fstart <= fend)
-	{
-		string[i] = s1[fstart];
-		i++;
-		fstart++;
-	}
-	string[i] = '\0';
-	return (string);
+	while (len > 0 && ft_clean(s1[len - 1], set))
+		len--;
+	if (len < i)
+		len = i;
+	if (!(strtrim = (char *)malloc(sizeof(char) * (len - i + 1))))
+		return (0);
+	while (i < len)
+		strtrim[j++] = (char)s1[i++];
+	strtrim[j] = 0;
+	return (strtrim);
 }
