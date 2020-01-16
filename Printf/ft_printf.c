ac23c0b1d1c20ccad8ef1a44d9020d07da53bb5e
Daniel Tellez llinas <dtellez-@c2r9s1.42madrid.com>
2020-01-16 15:44:55 +0100

    asd

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dtellez- <dtellez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 14:04:39 by dtellez-          #+#    #+#             */
-/*   Updated: 2020/01/14 16:38:01 by dtellez-         ###   ########.fr       */
+/*   Updated: 2020/01/16 15:43:55 by dtellez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 int		ft_printf(const char *fmt, ...)
 {
 	t_printf e;
+
 	e.fmt = (char *)fmt;
-	
 	e.len = 0;
 	va_start(e.ap, fmt);
 	while (*e.fmt != '\0')
@@ -45,10 +45,10 @@ int		ft_printf(const char *fmt, ...)
 			}
 			ft_search(&e);
 			e.fmt++;
-		}	
+		}
 		else
 		{
-			write (1, e.fmt, 1);
+			write(1, e.fmt, 1);
 			e.len++;
 			e.fmt++;
 		}
@@ -57,65 +57,8 @@ int		ft_printf(const char *fmt, ...)
 	return (e.len);
 }
 
-int		main()
+/*int		main()
 {
-/*	char s = 'y';
-	int x = 0;
-	int y = 0;
-
-	x = printf("Printf ori:\n%c\n", s);
-	printf("%d\n", x);
-	y = ft_printf("Printf mio:\n%c\n", s);
-	printf("%d\n", y);
-*/
-/*
-	char *s = "yusmsd";
-	int x = 0;
-	int y = 0;
-
-	x = printf("Printf ori:\n%s\n", s);
-	printf("%d\n", x);	
-	y = ft_printf("Printf mio:\n%s\n", s);
-	printf("%d\n", y);
-*/
-/*
-	int s = 123994823;
-	int x = 0;
-  	int y = 0;
-
-	x = printf("Printf ori:\n%d\n", s);
-	printf("%d\n", x);
-	y = ft_printf("Printf mio:\n%d\n", s);
-	printf("%d\n", y);
-*/
-/*	int s = -5555;
-	int x = 0;
-	int y = 0;
-
-	x = printf("Printf ori:\n%u\n", s);
-	printf("%d\n", x);
-	y = ft_printf("Printf mio:\n%u\n", s);
-	printf("%d\n", y);
-*/
-/*	unsigned int s = 10;
-	int x = 0;
-	int y = 0;
-
-	x = printf("Printf ori:\n%x\n", s);
-	printf("%d\n", x);
-	y = ft_printf("Printf mio:\n%x\n", s);
-	printf("%d\n", y);
-*/
-/*
-	unsigned int s = 10;
-	int x = 0;
-	int y = 0;
-
-	x = printf("Printf ori:\n%p\n", &s);
-	printf("%d\n", x);
-	y = ft_printf("Printf mio:\n%p\n", &s);
-	printf("%d\n", y);
-*/
 	int s = 123;
 	int x = 0;
 	int y = 0;
@@ -144,4 +87,4 @@ int		main()
 	y = ft_printf("Printf mio:\n|%030d|\n", s);
 	printf("%d\n\n", y);
 	
-}
+}*/
