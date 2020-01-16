ef53c995863586d8afc5cb25d552892aaf542fab
Daniel Tellez llinas <dtellez-@c2r9s1.42madrid.com>
2020-01-16 15:33:41 +0100

    asdasd

--- Printf/ft_printf_int.c
+++ Printf/ft_printf_int.c
@@ -6,7 +6,7 @@
 /*   By: dtellez- <dtellez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 18:26:20 by dtellez-          #+#    #+#             */
-/*   Updated: 2020/01/14 16:40:31 by dtellez-         ###   ########.fr       */
+/*   Updated: 2020/01/16 15:32:02 by dtellez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,39 @@ char	*ft_string_create(int size_str, char c, char *str)
 	return (str);
 }
 
+void	ft_all_conditionals(t_printf *e, char *str_aux, char **str,
+int is_negative)
+{
+	int size_str;
+
+	size_str = e->p - e->len_swap;
+	if (size_str > 0)
+	{
+		str_aux = ft_string_create(size_str, '0', str_aux);
+		*str = ft_strjoin(str_aux, *str);
+	}
+	(is_negative == 1) ? *str = ft_strjoin("-", *str) : 0;
+	size_str = e->w - e->p;
+	if (size_str > 0 && e->text_left == 0 && e->text_zero == 0)
+	{
+		str_aux = ft_string_create(size_str, ' ', str_aux);
+		*str = ft_strjoin(str_aux, *str);
+	}
+	else if (size_str > 0 && e->text_zero == 1)
+	{
+		str_aux = ft_string_create(size_str, '0', str_aux);
+		*str = ft_strjoin(str_aux, *str);
+	}
+	else if (size_str > 0 && e->text_left == 1)
+	{
+		str_aux = ft_string_create(size_str, ' ', str_aux);
+		*str = ft_strjoin(*str, str_aux);
+	}
+}
+
 char	*ft_string_aux(t_printf *e)
 {
 	int		i;
-//	char 	*str;
-	int		size_str;
 	char	*str_aux;
 	char	*str;
 	int		is_negative;
@@ -51,32 +79,7 @@ char	*ft_string_aux(t_printf *e)
 		str++;
 	}
 	ft_swap(str, e);
-	size_str = e->p - e->len_swap;
-	if (size_str > 0)
-	{
-		str_aux = ft_string_create(size_str, '0', str_aux);
-		str = ft_strjoin(str_aux, str);
-	}
-	if (is_negative == 1)
-	{
-		str = ft_strjoin("-", str);
-	}
-	size_str = e->w - e->p;
-	if (size_str > 0 && e->text_left == 0 && e->text_zero == 0)
-	{	
-		str_aux = ft_string_create(size_str, ' ', str_aux);
-		str = ft_strjoin(str_aux, str);
-	}
-	else if (size_str > 0 && e->text_zero == 1)
-	{
-		str_aux = ft_string_create(size_str, '0', str_aux);
-		str = ft_strjoin(str_aux, str);
-	}
-	else if (size_str > 0 && e->text_left == 1)
-	{
-		str_aux = ft_string_create(size_str, ' ', str_aux);
-		str = ft_strjoin(str, str_aux);
-	}
+	ft_all_conditionals(e, str_aux, &str, is_negative);
 	return (str);
 }
 
