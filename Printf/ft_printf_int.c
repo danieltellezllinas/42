9bbb1808b8350e0b4fa329e95ea6d005abf94fde
Daniel Tellez llinas <dtellez-@c2r10s2.42madrid.com>
2020-01-17 16:31:00 +0100

    asd

--- Printf/ft_printf_int.c
+++ Printf/ft_printf_int.c
@@ -6,7 +6,7 @@
 /*   By: dtellez- <dtellez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 18:26:20 by dtellez-          #+#    #+#             */
-/*   Updated: 2020/01/17 15:20:45 by dtellez-         ###   ########.fr       */
+/*   Updated: 2020/01/17 16:30:36 by dtellez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,26 @@ char	*ft_string_create(int size_str, char c, char *str)
 	return (str);
 }
 
+void	ft_create_zero_and_space(char **str_aux, int size_str,
+		char simbol, char **str)
+{
+	*str_aux = ft_string_create(size_str, simbol, *str_aux);
+	*str = ft_strjoin(*str_aux, *str);
+}
+
 void	ft_all_conditionals(t_printf *e, char *str_aux, char **str,
 int is_negative)
 {
 	int size_str;
+
 	size_str = e->p - e->len_swap;
 	if (size_str > 0)
-	{
-		str_aux = ft_string_create(size_str, '0', str_aux);
-		*str = ft_strjoin(str_aux, *str);
-	}
+		ft_create_zero_and_space(&str_aux, size_str, '0', str);
 	(is_negative == 1) ? *str = ft_strjoin("-", *str) : 0;
 	size_str = e->w - e->p;
 	(is_negative == 1) ? size_str -= 1 : 0;
 	if (size_str > 0 && e->text_left == 0 && e->text_zero == 0)
-	{
-		str_aux = ft_string_create(size_str, ' ', str_aux);
-		*str = ft_strjoin(str_aux, *str);
-	}
+		ft_create_zero_and_space(&str_aux, size_str, ' ', str);
 	else if (size_str > 0 && e->text_zero == 1)
 	{
 		str_aux = ft_string_create(size_str, '0', str_aux);
--- Printf/libftprintf.h
+++ Printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: dtellez- <dtellez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 13:37:07 by dtellez-          #+#    #+#             */
-/*   Updated: 2020/01/16 19:40:25 by dtellez-         ###   ########.fr       */
+/*   Updated: 2020/01/17 16:23:24 by dtellez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ void            ft_calculate_width(t_printf *e);
 void			ft_swap(char *str, t_printf *e);
 void			ft_reset_values(t_printf *e);
 char			*ft_string_create(int size_str, char c, char *str);
-
+void			ft_create_zero_and_space(char **str_aux, int size_str,
+				char simbol, char **str);
 #endif
