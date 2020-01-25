e50cf62b45d152e5f06b071eae28eb28f80bc9da
Daniel Tellez llinas <dtellez-@c2r10s1.42madrid.com>
2020-01-25 14:03:43 +0100

    asd

--- Printf/ft_printf_memory_direction.c
+++ Printf/ft_printf_memory_direction.c
@@ -6,7 +6,7 @@
 /*   By: dtellez- <dtellez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 18:28:18 by dtellez-          #+#    #+#             */
-/*   Updated: 2020/01/25 13:57:11 by dtellez-         ###   ########.fr       */
+/*   Updated: 2020/01/25 14:02:32 by dtellez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,15 +71,12 @@ char	*ft_string_aux_memory(t_printf *e, unsigned long int i,
 	{
 		e->i_is_zero = 1;
 		str = (!e->p) ? ft_strdup("0x") : ft_strdup("0x0");
-		if (e->w > (int)ft_strlen(str) && e->text_left == 1)
+		if (e->w > (int)ft_strlen(str))
 		{
-			str_aux = ft_string_create_memory(e->w - ft_strlen(str), ' ', str_aux);
-			str = ft_strjoin(str, str_aux);
-		}
-		else if (e->w > (int)ft_strlen(str) && e->text_left == 0)
-		{
-			str_aux = ft_string_create_memory(e->w - ft_strlen(str), ' ', str_aux);
-			str = ft_strjoin(str_aux, str);
+			str_aux = ft_string_create_memory(e->w - ft_strlen(str), ' ',
+			str_aux);
+			str = (e->text_left == 1) ? ft_strjoin(str, str_aux) :
+			ft_strjoin(str_aux, str);
 		}
 	}
 	else
@@ -108,7 +105,6 @@ void	ft_printf_memory_direction(t_printf *e)
 		e->text_left = 1;
 		e->text_zero = 0;
 	}
-
 	str_aux = 0;
 	is_negative = 0;
 	i = 0;
--- Printf/mainprueba.c
+++
@@ -1,8 +0,0 @@
-#include "libftprintf.h"
-#include <stdio.h>
-
-int main ()
-{
-    printf("ORI: %p\n", (void *)0xe70d68e24);
-    ft_printf("MIO:%p\n", (void *)0xe70d68e24);
-}
